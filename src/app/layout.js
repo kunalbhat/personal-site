d146864e44b00bc2e14823a5e3fb2c67cc09da67
kunalbhat <kunalbhat@gmail.com>
2025-04-16 21:23:49 -0500

    add og stuff

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,22 @@ export const metadata = {
   title: "Kunal Bhat - Product Designer",
   description:
     "Kunal is a technically-oriented, fullstack product designer in Chicago with 15 years of experience.",
+  openGraph: {
+    title: "Kunal Bhat - Product Designer",
+    description:
+      "Kunal is a technically-oriented, fullstack product designer in Chicago with 15 years of experience.",
+    url: "https://kunalbhat.com",
+    siteName: "Kunal Bhat",
+    images: [
+      {
+        url: "https://kunalbhat.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Kunal Bhat - Product Designer",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
