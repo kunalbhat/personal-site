238d86b6fe89baf5e41f168043b604a12354e282
kunalbhat <kunalbhat@gmail.com>
2025-10-21 22:52:40 -0500

    small copy updates

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -18,17 +18,21 @@ export default function About() {
         </p>
         <p>
           Prior to that, by way of <a href="#">Magic+Might</a>, I dove deep into
-          the autonomous ride-hail space with <a href="#">Waymo</a>.
+          the autonomous ride-hail space with <a href="#">Waymo</a>,
+          understanding rider sentiments and designing for future states in the
+          Waymo app.
         </p>
         <p>
           2021 &ndash; 2023 I spent 2 years at Galactic, an angel-backed
           startup, exploring crypto and building tools for web3 users.
         </p>
         <p>
-          Prior to that, I spent 6 years at PayPal building merchant experiences
-          spanning from <a href="#">Braintree's Control Panel</a> to leading
-          Braintree's Identity infrastructure .
+          2015 &ndash; 2021 I spent 6 years at PayPal building merchant
+          experiences spanning from <a href="#">Braintree's Control Panel</a> to
+          leading Braintree's Identity team during PayPal's platform unification
+          efforts.
         </p>
+        <p>2014 &ndash; 2015 I led design at Modest </p>
       </main>
     </>
   );
